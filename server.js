30cb647f7d2170f10e49bd7aed57ddd1f806c867
Igweachi Jesse <104082049+obaikoko@users.noreply.github.com>
2022-09-30 21:28:00 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const color = require('colors')
+const connectDB = require('./config/db')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+const app = express()
+connectDB() 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
+app.use(errorHandler)
+
+app.listen(port, console.log(`Server running on port ${port}`))
